7fc1ec3ef23e7e8b5d1b36665a7a8829055d2fc7
Tanner Norris <jiujitsucarrot@Tanners-MacBook-Air.local>
2023-11-06 14:16:17 -0700

    Added starter code to js files.

---
+++ src/components/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <section>
+            <h2>Contact</h2>
+            <form>
+
+            </form>
+        </section>
+    );
+}
+
+export default Contact;
