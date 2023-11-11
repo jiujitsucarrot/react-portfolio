34b374a3143943b806d44838f7b9e8696290083c
Tanner Norris <jiujitsucarrot@Tanners-MacBook-Air.local>
2023-11-10 19:25:05 -0700

    Adjusted App.jsx file trying to debug code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Header from './components/Header';
-import Navigation from './components/Navigation';
-import AboutMe from './components/AboutMe';
-import Portfolio from './components/Portfolio';
-import Contact from './components/Contact';
-import Resume from './components/Resume';
-import Footer from './components/Footer';
+import Header from './components/Header.jsx';
+import Navigation from './components/Navigation.jsx';
+import AboutMe from './components/AboutMe.jsx';
+import Portfolio from './components/Portfolio.jsx';
+import Contact from './components/Contact.jsx';
+import Resume from './components/Resume.jsx';
+import Footer from './components/Footer.jsx';
 
 const App = () => {
     return (
