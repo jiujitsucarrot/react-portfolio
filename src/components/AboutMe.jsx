b5ab63e21b32a0cf79ea6a54d4b3d30e64245a23
Tanner Norris <jiujitsucarrot@Tanners-MacBook-Air.local>
2023-11-10 18:39:11 -0700

    Added to AboutMe and Header.jsx. Still debugging.

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AboutMe = () => {
+    return (
+        <div className="about-me">
+            <h2>About Me</h2>
+            <img src="/assets/images/avatar.jpg" alt="Your Avatar" />
+            <p>
+                **************
+            </p>
+        </div>
+    );
+};
+
+export default AboutMe;
