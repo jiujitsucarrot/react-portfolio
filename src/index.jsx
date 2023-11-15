5802e45bbc3a577f404daa8c8ab34bbc61084cf2
Tanner Norris <jiujitsucarrot@Tanners-MacBook-Air.local>
2023-11-14 19:52:45 -0700

    Adjusted index.jsx connection, built dist.

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+{ Link }createRoot(document.getElementById('root')).render(
         <App />
 )
